e5aa13741e79d81a607b333955099a873dcc73fd
Nilotpaul Nandi <nilotpaul.nandi2021@uem.edu.in>
2023-08-15 17:30:23 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
       "/api/entries": {
         target: "http://localhost:5000",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
